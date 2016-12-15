266e9cf23467afcb9e66842907fd16740eb81e1e
Subhra-Barpanda <luv2bwith@gmail.com>
2016-12-15 13:43:17 +0530

    Delete GraphAPIInterface.java

--- src/src/org/coursera/www/princetonuniversity/part2/week1/undirectedgraphs/GraphAPIInterface.java
+++
@@ -1,9 +0,0 @@
-package org.coursera.www.princetonuniversity.part2.week1.undirectedgraphs;
-
-public interface GraphAPIInterface {
-
-	public void addEdge(int fromVertex,int toVertex);
-	public Iterable<Integer> iterateOverAdjVertices(int sourceEdge);
-	public int getNoOfVertices();
-	public int getNoOfEdges();
-}
