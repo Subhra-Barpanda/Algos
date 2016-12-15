41604dbe2423044feabb90fd8ba54c5af3fa3e2e
Subhra-Barpanda <luv2bwith@gmail.com>
2016-12-15 13:40:14 +0530

    Delete GenericStack.java

--- src/src/org/coursera/www/princetonuniversity/part1/week2/stacksandqueues/GenericStack.java
+++
@@ -1,8 +0,0 @@
-package org.coursera.www.princetonuniversity.part1.week2.stacksandqueues;
-
-public interface GenericStack<E> {
-	public void push(E entity);
-	public E pop();
-	public E top();
-	
-}
